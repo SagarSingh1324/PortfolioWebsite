14e42a5b8c3d1336a47c1b1ebfb6c78217c96153
SagarSingh1324 <sagarsinghprj@gmail.com>
2025-07-31 16:34:11 +0530

    removed unused package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import React from 'react'
-
 import Sidebar from './Componenets/Sidebar/Sidebar'
 import About from './Componenets/About/About'
 import TechStack from './Componenets/TechStack/TechStack'
 import Projects from './Componenets/Projects/Projects'
-// import WorkExperience from './Componenets/WorkExperience/WorkExperience'
 import Contact from './Componenets/Contact/Contact'
 import Education from './Componenets/Education/Education'
-
-// import Testimonial from './Componenets/Testimonial/Testimonial'
 import ScrollToTop from 'react-scroll-to-top'
 import './App.css'
 
@@ -19,9 +14,7 @@ const App = () => {
       <About />
       <Projects />
       <TechStack />
-      {/* <WorkExperience /> */}
       <Education />
-      {/* <Testimonial /> */}
       <Contact />
 
       <ScrollToTop smooth={true}
